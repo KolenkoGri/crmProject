0c316d289252d3d6c0a7b9435930cadfd5a38760
KolenkoGri <kolenkogrigorenko@gmail.com>
2023-05-15 20:04:29 +0400

    completed lesson5_2

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const modalId = document.querySelector('.modal__id');
+const modalClose = document.querySelector('.modal__close');
+const modal = document.querySelector('.modal');
+const modalCheck = document.querySelector('.form__input--checkbox');
+const modalCheckInput = document.querySelector('.form__checking .form__input');
+const totalCount = document.querySelectorAll('tbody tr td:nth-child(7)');
